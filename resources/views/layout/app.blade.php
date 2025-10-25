15611494c24a82db5b4add6603eef7550d9c8f73
calcudaus <dausg878@gmail.com>
2025-10-25 21:50:58 +0700

    Refactor code structure for improved readability and maintainability

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,10 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <aside>
-        
+        <div class="logo">
+            <img src="{{ asset("assets/img/logo-project.png") }}" width="44px" alt="" >
+        </div>
+        <nav>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
     </aside>
     <main>
         <header>

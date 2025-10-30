818d0c585a1e5e7040b1ccfe172832d08ca55918
calcudaus <dausg878@gmail.com>
2025-10-30 23:20:22 +0700

    Add route for HelloWorld service to greet users

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Services\HelloWorld;
+
 
 Route::get('/', function () {
     return view('layout.app');
+});
+Route::get('/hello', function (HelloWorld $hello) {
+    dd($hello->greet());
 });
